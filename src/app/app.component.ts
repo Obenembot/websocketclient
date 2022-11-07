8d9fd5ba77ff56c7cb0eaf683434399a7cc2bd85
sampson.obenembot <sampson.obenembot@sgits.co.za>
2022-11-07 20:27:36 +0200

    websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import {CompatClient, Stomp} from "@stomp/stompjs";
+// @ts-ignore
+import * as SockJS from 'sockjs-client';
+import {Subscription} from "rxjs";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wesocket';
+  title = 'angular8-springboot-websocket';
+
+  greeting: any;
+
+  ngOnInit() {
+    this.connect();
+  }
+
+
+  stompClient: CompatClient | any;
+  private stompClientSubscription: Subscription = new Subscription();
+
+  connect(): void {
+    const socket = new SockJS('http://localhost:8080/ws');
+    this.stompClient = Stomp.over(socket);
+    this.stompClient.connect({}, (frame: { command: string; }) => {
+      if (frame.command === 'CONNECTED') {
+         let topic = "/topic/greetings";
+        this.stompClientSubscription = this.stompClient.subscribe(topic, (alertJSON: any) => {
+          const alert = JSON.parse(alertJSON.body);
+          this.greeting = alert.name;
+          console.log('message from web socket', alert)
+        });
+      }
+    });
+  }
+
 }
